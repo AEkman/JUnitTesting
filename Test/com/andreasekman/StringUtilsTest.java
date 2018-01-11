8b8a728cad213a0e8a005a582b8419e9463581f9
Andreas Ekman <info@andreasekman.com>
2018-01-11 15:57:58 +0100

    String utils

---
+++ Test/com/andreasekman/StringUtilsTest.java
@@ -0,0 +1,66 @@
+package com.andreasekman;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringUtilsTest {
+
+    // This one fails
+    @Test
+    public void testReverseWithEscapeCharacters() {
+
+        String test = "\uD800\uDC00";
+        String result = StringUtils.reverse("\uD800\uDC00");
+
+        assertEquals(result, new StringBuilder(test).reverse().toString());
+    }
+
+    @Test
+    public void testReverse() throws Exception {
+
+        String result = StringUtils.reverse("test");
+
+        assertEquals(result, "tset");
+    }
+
+    @Test
+    public void testReverseEmptyString() {
+
+        String result = StringUtils.reverse("");
+
+        assertEquals(result, "");
+    }
+
+    @Test
+    public void testReverseWhiteSpace() {
+
+        String result = StringUtils.reverse("");
+
+        assertEquals(result, "");
+    }
+
+    @Test
+    public void testReverseConcatination() {
+
+        String result = StringUtils.reverse("x" +"y");
+
+        assertEquals(result, "y" + "x");
+    }
+
+
+
+    @Test
+    public void testReverseWithDifferentCasing() {
+
+        String result = StringUtils.reverse("Ok");
+
+        assertEquals(result, "kO");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testReverseThrowsExceptionsWhenAddingNull(){
+        StringUtils.reverse(null);
+    }
+
+}
---
+++ src/com/andreasekman/StringUtils.java
@@ -0,0 +1,23 @@
+package com.andreasekman;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringUtils {
+
+    public static String reverse(String s) {
+
+        if(s == null) throw new NullPointerException();
+
+        List<String> tempArray = new ArrayList<String>(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            tempArray.add(s.substring(i, i + 1));
+        }
+
+        StringBuilder reversedString = new StringBuilder(s.length());
+        for (int i = tempArray.size() - 1; i >= 0; i--) {
+            reversedString.append(tempArray.get(i));
+        }
+        return reversedString.toString();
+    }
+}
